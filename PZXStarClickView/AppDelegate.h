bc2d1f5fd8cf7180512cdb828c35ec8dbc179390
彭祖鑫 <pengzuxin@pengzuxindeiMac.local>
2017-01-09 09:19:23 +0800

    Initial Commit

---
+++ PZXStarClickView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZXStarClickView
+//
+//  Created by 彭祖鑫 on 2017/1/9.
+//  Copyright © 2017年 PZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PZXStarClickView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PZXStarClickView
+//
+//  Created by 彭祖鑫 on 2017/1/9.
+//  Copyright © 2017年 PZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
